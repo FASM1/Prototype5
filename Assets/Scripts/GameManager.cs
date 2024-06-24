d7ff50533d7bacbb802596b5aba60285df1a9608
FASM1 <filipeari@live.com.pt>
2024-06-24 12:13:03 +0100

    7.Show restart button on game over
    7.Show restart button on game over@38261

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour
@@ -13,6 +14,7 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
+    public Button restartButton;
     // Start is called before the first frame update
     void Start() 
     {
@@ -48,9 +50,11 @@ public class GameManager : MonoBehaviour
     {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
     }
     public void RestartGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+    
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
